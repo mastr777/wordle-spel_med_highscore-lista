4c4b7c2e6e4a42938f679ea37bb944871c325a18
mastr777 <mstrandberg777@gmail.com>
2026-04-10 11:32:38 +0200

    add wordlist for experimenting

---
+++ server/words.js
@@ -0,0 +1,19 @@
+// list of different words - experimental test
+const words = [
+
+  "pear",
+  "banana",
+  "monitor",
+  "bubblegum",
+  "stone",
+  "shoes",
+  "water",
+  "stone",
+  "light",
+  "keyboard"
+
+];
+
+module.exports = words;
+
+
