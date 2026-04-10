8fdcc2e8243e12cd52e1c718ea9a450120f2f32e
mastr777 <mstrandberg777@gmail.com>
2026-04-10 14:41:56 +0200

    small edit

--- server/index.js
+++ server/index.js
@@ -1,22 +1,34 @@
 const express = require("express");
 const path = require("path");
+const words = require("./words");
 
 const app = express();
 const PORT = 5080;
 
-// Serve React build
 app.use(express.static(path.join(__dirname, "../wordle-game/dist")));
+
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../wordle-game/dist/index.html"));
+
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
+
 });
 
 app.get("/api/word", (req, res) => {
-  res.json({ word: "apple" });
+
+  const randomWord = match.floor(Math.random() * words.length);
+  const word = words[randomWord];
+
+  res.json({ word });
+
 });
+
+
+
 
 
