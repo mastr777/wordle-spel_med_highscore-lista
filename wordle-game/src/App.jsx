44a71088cf3616fb5ba3e68efb994f22c3c2b079
mastr777 <mstrandberg777@gmail.com>
2026-04-11 14:43:57 +0200

    add new code to gamepage - input field and other changes

--- wordle-game/src/App.jsx
+++ wordle-game/src/App.jsx
@@ -1,27 +1,56 @@
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function App() {
 
   const [word, setWord] = useState("");
+  const [length, setLength] = useState(5);
+  const [unique, setUnique] = useState(true);
 
-  useEffect(() => {
-    fetch("/api/word?length=5")
-      .then(res => res.json())
-      .then(data => setWord(data.word));
-  }, []);
+  const getWord = () => {
+
+    fetch(`/api/word?length=${length}&unique=${unique}`)
+      .then((res) => res.json())
+      .then((data) => setWord(data.word));
+      
+  };
 
   return (
-
     <div>
-      <h1>Wordle</h1>
-      <p>The Word: {word}</p>
-    </div>
 
+      <h1>Wordle</h1>
+
+      <div>
+
+        <span>
+          Word length:
+          <input type="number" value={length} onChange={(e) => setLength(e.target.value)} />
+        </span>
+
+      </div>
+
+      <div>
+
+        <p>Choose type of word:</p>
+        <span>
+          Unique letters 'check':
+          <input type="checkbox" checked={unique} onChange={(e) => setUnique(e.target.checked)} />
+        </span>
+
+      </div>
+
+      <button onClick={getWord}>Start Game</button>
+
+      <p>Word: {word}</p>
+    </div>
   );
 }
 
 export default App;
+
+
+
+
 
 
 
