e15021f68f14bdf56a2efe58fcbfb1ee538ff2bc
mastr777 <mstrandberg777@gmail.com>
2026-04-11 17:25:50 +0200

    make the word and guess lowercase - check if right

--- server/index.js
+++ server/index.js
@@ -38,18 +38,60 @@ app.get("/api/word", (req, res) => {
   }
 
   if (wordFiltering.length === 0) {
-    return res.status(400).json({ error: "No words found with that length" });
+    return res.status(400).json({ error: "No Word with that length" });
   }
+
+function getFinalization(guess, word) {
+  const result = [];
+  const wordLetters = word.split("");
+  const guessLetters = guess.split("");
+
+  for (let i = 0; i < guessLetters.length; i++) {
+    if (guessLetters[i] === wordLetters[i]) {
+
+      result[i] = { letter: guessLetters[i], status: "correct" };
+
+      wordLetters[i] = null;
+      guessLetters[i] = null;
+
+    }
+  }
+
+  for (let i = 0; i < guessLetters.length; i++) {
+    if (guessLetters[i] === null) continue;
+
+    const index = wordLetters.indexOf(guessLetters[i]);
+
+    if (index !== -1) {
+      result[i] = { letter: guessLetters[i], status: "misplaced" };
+      wordLetters[index] = null;
+    } else {
+      result[i] = { letter: guessLetters[i], status: "incorrect" };
+    }
+  }
+
+  return result;
+}
+
 
 app.post("/api/guess", (req, res) => {
   const { guess, word } = req.body;
 
   if (!guess || !word) {
-    return res.status(400).json({ error: "Guess and word are required" });
+    return res.status(400).json({ error: "A guess and a Word are required" });
   }
-  const isCorrect = guess.toLowerCase() === word.toLowerCase();
 
-  res.json({ isCorrect });
+  if (guess.length !== word.length) {
+    return res.status(400).json({ error: "Guess must have the same length as the Word"});
+  }
+
+  const lowerGuess = guess.toLowerCase();
+  const lowerWord = word.toLowerCase();
+  const finalization = getFinalization(lowerGuess, lowerWord);
+
+  const isCorrect = lowerGuess === lowerWord;
+
+  res.json({ isCorrect, finalization });
   
 })
 
